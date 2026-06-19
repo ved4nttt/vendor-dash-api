8dff2132015b0c328ca63aba36cbc1fc6562cccb
vedant <vedantparabw11@gmail.com>
2026-06-19 12:52:30 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# In-memory database for Vendor Dash
+inventory = {
+    "101": {"name": "Wireless Mouse", "stock": 45, "price": 25.99},
+    "102": {"name": "Mechanical Keyboard", "stock": 12, "price": 85.00}
+}
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Vendor Dash API is live and running via Docker!"})
+
+@app.route('/inventory', methods=['GET'])
+def get_inventory():
+    return jsonify(inventory)
+
+@app.route('/inventory/add', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    item_id = data.get('id')
+    inventory[item_id] = {
+        "name": data.get('name'),
+        "stock": data.get('stock'),
+        "price": data.get('price')
+    }
+    return jsonify({"message": "Item added to storefront", "item": inventory[item_id]}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
